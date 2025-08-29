831e367524ca72e446a4e08ef496099a302f64b2
Vergiliogu <vergiliopoleza@gmail.com>
2025-08-28 21:18:04 -0300

    init commit

---
+++ project03/src/main/java/com/mycompany/project03/Adapters/ArCondicionadoAdapter.java
@@ -0,0 +1,6 @@
+package com.mycompany.project03.Adapters;
+
+public interface ArCondicionadoAdapter {
+    void ligar();
+    void desligar();
+}
---
+++ project03/src/main/java/com/mycompany/project03/Adapters/LampadaAdapter.java
@@ -0,0 +1,6 @@
+package com.mycompany.project03.Adapters;
+
+public interface LampadaAdapter {
+    void ligar();
+    void desligar();
+}
